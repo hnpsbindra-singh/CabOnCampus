db76664e25ccb69fb3ec5b5fe51aba983775ce85
Harnimarpreet Singh Bindra <hnps.bindra@gmail.com>
2025-11-08 03:48:56 +0530

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // your backend URL
+  baseURL: "https://cab-on-campus-ioh3.vercel.app/", // your backend URL
 });
 
 export default api;
+
